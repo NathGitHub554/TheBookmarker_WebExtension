c81deee6a91503901f8ed54030959b372c2c1bdc
NathGitHub554 <npbayne@gmail.com>
2026-08-21 14:17:38 -0400

    added comments to JS file about next steps for development, things to research more.

--- Website_Bookmarker/bookmarker_main.js
+++ Website_Bookmarker/bookmarker_main.js
@@ -1,4 +1,4 @@
-//read more of JS Selection API
+
 function start() {
   // Source - https://stackoverflow.com/a/26308552
   // Posted by chintan adatiya, modified by community. See post 'Timeline' for change history
@@ -6,6 +6,8 @@ function start() {
   const btn = document.querySelector("#btn");
 
   //check out CONTENT SCRIPTS, which run on the actual page that's being visited: https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
+
+  //also, have 2 scripts: one for popup, that manages button and adding elements (is linked in the popup itself), and one for running on the page (content script, it isn't linked to the popup, it runs separately) where it would grab the parent element and ID. Messages would be passed between the two scripts.
   function saveBookmark() {
         //define link same as above.
     let pageLink = window.location.href;
