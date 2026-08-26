16412c3194a97e23f26675248ee2d3abf6390990
NathGitHub554 <npbayne@gmail.com>
2026-08-25 21:52:22 -0400

    researched message passing and updated notes in file.

--- Website_Bookmarker/bookmarker_main.js
+++ Website_Bookmarker/bookmarker_main.js
@@ -5,9 +5,17 @@ function start() {
   // Retrieved 2026-08-19, License - CC BY-SA 3.0
   const btn = document.querySelector("#btn");
 
-  //check out CONTENT SCRIPTS, which run on the actual page that's being visited: https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
+  // Content Scripts: https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
 
-  //also, have 2 scripts: one for popup, that manages button and adding elements (is linked in the popup itself), and one for running on the page (content script, it isn't linked to the popup, it runs separately) where it would grab the parent element and ID. Messages would be passed between the two scripts.
+  // also, have 2 scripts: one for popup, that manages button and adding elements (is linked in the popup itself), and one for running on the page (content script, it isn't linked to the popup, it runs separately) where it would grab the parent element and ID. Messages would be passed between the two scripts.
+
+  // Chrome Message Passing Docs: https://developer.chrome.com/docs/extensions/develop/concepts/messaging
+  // Program Flow: 
+  // 1. User selects text, clicks "save bookmark."
+  // 2. Popup JS sends message to content script to grab ID
+  // 3. Content script grabs ID and sends response with ID
+  // 4. Extension script recieves ID and makes link, saves link in Chrome storage API. (Link: https://developer.chrome.com/docs/extensions/reference/api/storage)
+
   function saveBookmark() {
         //define link same as above.
     let pageLink = window.location.href;
